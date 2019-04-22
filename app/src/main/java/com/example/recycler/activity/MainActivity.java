ef874f597a927806f71da6d299d830f0a4772d72
vuleptit <vule268vp@gmail.com>
2019-04-22 10:26:20 +0700

    Changes: MainActivity, nav_header.xml, menu.xml Created: FacebookLogin.java, activity_facebok_login.xml, User(Entity), GetUserCallBack(dao1), UserRequest (dao1) nav_header_main.xml -> nav_header

--- app/src/main/java/com/example/recycler/activity/MainActivity.java
+++ app/src/main/java/com/example/recycler/activity/MainActivity.java
@@ -1,12 +1,10 @@
 package com.example.recycler.activity;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
-import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -14,11 +12,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ImageView;
-import android.widget.Toast;
+import android.widget.TextView;
 
+import com.example.recycler.FacebookLogin;
 import com.example.recycler.R;
 import com.example.recycler.fragment.*;
+import com.facebook.AccessToken;
+import com.facebook.login.LoginManager;
+import com.facebook.login.widget.ProfilePictureView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private  DrawerLayout drawerLayout;
@@ -27,15 +32,55 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private FragmentDownload fragmentDownload;
     private int check = 0;
     private FragmentManager fragmentManager;
+    private AccessToken accessToken;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_test);
+        setContentView(R.layout.activity_test);//cài đặt layout được hiển thị
+        accessToken = AccessToken.getCurrentAccessToken();
+
+
+//            NavigationView navHeader = (NavigationView) findViewById(R.id.navigation_main_login);
+//            View hView = navHeader.getHeaderView(0);
+//            TextView userName = (TextView) hView.findViewById(R.id.user_text_login);
+//            userName.setText("Lê Hồng Vũ");
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_main_login);
+        View headerView = navigationView.getHeaderView(0);
+        if(accessToken != null){
+            navigationView.getMenu().findItem(R.id.nav_login).setVisible(false);
+            navigationView.getMenu().findItem(R.id.nav_logout).setVisible(true);
+            TextView userName = (TextView) headerView.findViewById(R.id.user_text_login);
+            ProfilePictureView uPic = (ProfilePictureView) headerView.findViewById(R.id.profile_image);
+//            userPicture.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+            Intent intent = getIntent();
+
+            if(intent != null){
+                Bundle extras = intent.getExtras();
+                if(extras != null){
+                    String isLogged = extras.getString("UName", "Nope");
+                    if(!isLogged.equals("Nope")){
+                        userName.setText(intent.getExtras().getString("UName"));
+                        uPic.setProfileId(intent.getExtras().getString("UID"));
+                    }
+                }else{
+                    navigationView.getMenu().findItem(R.id.nav_login).setVisible(true);
+                    navigationView.getMenu().findItem(R.id.nav_logout).setVisible(false);
+                }
+            }
+        }else{
+            navigationView.getMenu().findItem(R.id.nav_login).setVisible(true);
+            navigationView.getMenu().findItem(R.id.nav_logout).setVisible(false);
+            TextView userName = (TextView) headerView.findViewById(R.id.user_text_login);
+            userName.setText("User name");
+        }
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        navigationView = findViewById(R.id.navigation_main);
+        navigationView = findViewById(R.id.navigation_main_login);
         ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(drawerToggle);
         drawerToggle.setDrawerIndicatorEnabled(true);
@@ -45,8 +90,45 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         if(navigationView!=null){
             navigationView.setNavigationItemSelectedListener(this);
         }
+
         getSupportFragmentManager().beginTransaction().replace(R.id.frame_home,new FragmentHome()).commit();
+
+
+//lấy keyhash của ứng dụng
+//        try {
+//            PackageInfo info = getPackageManager().getPackageInfo(
+//                    "com.example.recycler",
+//                    PackageManager.GET_SIGNATURES);
+//            for (Signature signature : info.signatures) {
+//                MessageDigest md = MessageDigest.getInstance("SHA");
+//                md.update(signature.toByteArray());
+//                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+//            }
+//        } catch (PackageManager.NameNotFoundException e) {
+//
+//        } catch (NoSuchAlgorithmException e) {
+//
+//        }
+
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setDisplayShowTitleEnabled(false);
+//        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        navigationView = findViewById(R.id.navigation_main);
+//        ActionBarDrawerToggle drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+//        drawerLayout.addDrawerListener(drawerToggle);
+//        drawerToggle.setDrawerIndicatorEnabled(true);
+//        drawerToggle.syncState();
+//        init();
+//
+//        if(navigationView!=null){
+//            navigationView.setNavigationItemSelectedListener(this);
+//        }
+//
 //        navigationView.getMenu().getItem(0).setChecked(true);
+
+
+
     }
 
 
@@ -65,7 +147,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         } else {
             if(check==0||check==R.id.nav_home){
                 super.onBackPressed();
-            }else {
+            }
+            else {
                 getSupportFragmentManager().beginTransaction().replace(R.id.frame_home,new FragmentHome()).commit();
                 check =0;
                 navigationView.getMenu().getItem(0).setChecked(true);
@@ -87,9 +170,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.nav_history:
                 getSupportFragmentManager().beginTransaction().replace(R.id.frame_home,new FragmentHistory()).commit();
                 break;
+            case R.id.nav_login:
+                Intent intent = new Intent(this, FacebookLogin.class);
+                startActivity(intent);
+                break;
+            case R.id.nav_logout:
+                LoginManager.getInstance().logOut();
+                Intent intentOut = new Intent(this , MainActivity.class);
+                startActivity(intentOut);
+                break;
         }
         check = id;
         drawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
+
+
